0d3e5e37eb679561dcb5f2fca7f9d5d8d94fac90
Xiaobin Zhang <david8862@gmail.com>
2019-08-19 19:28:31 +0800

    add more option for train process control

--- train.py
+++ train.py
@@ -4,7 +4,7 @@
 Retrain the YOLO model for your own dataset.
 """
 
-import os, argparse
+import os, random, argparse
 import numpy as np
 import tensorflow.keras.backend as K
 from tensorflow.keras.optimizers import Adam
@@ -23,6 +23,21 @@ session = tf.Session(config=config)
 
 # set session
 K.set_session(session)
+
+
+def get_multiscale_param(model_type, tiny_version):
+    # get input_shape & batch_size list for multiscale training
+    if (model_type == 'darknet' or model_type == 'xception') and not tiny_version:
+        # due to GPU memory limit, we could only use small input_shape and batch_size
+        # for full YOLOv3 and Xception models
+        input_shape_list = [(320,320), (416,416), (480,480)]
+        batch_size_list = [4, 8]
+    else:
+        input_shape_list = [(320,320), (416,416), (512,512), (608,608)]
+        batch_size_list = [4, 8, 16]
+
+    return input_shape_list, batch_size_list
+
 
 def _main(args):
     annotation_file = args.annotation_file
@@ -68,15 +83,11 @@ def _main(args):
     num_val = int(len(lines)*val_split)
     num_train = len(lines) - num_val
 
-    # get input_shape & batch_size list for multiscale training
-    if (args.model_type == 'darknet' or args.model_type == 'xception') and not args.tiny_version:
-        # due to GPU memory limit, we could only use small input_shape and batch_size
-        # for full YOLOv3 and Xception models
-        input_shape_list = [(320,320), (416,416), (480,480)]
-        batch_size_list = [4, 8]
+    if args.multiscale:
+        input_shape_list, batch_size_list = get_multiscale_param(args.model_type, args.tiny_version)
     else:
-        input_shape_list = [(320,320), (416,416), (512,512), (608,608)]
-        batch_size_list = [4, 8, 16]
+        input_shape_list = [args.model_image_size]
+        batch_size_list = [args.batch_size]
 
     model = get_yolo3_model(args.model_type, anchors, num_classes, weights_path=args.weights_path, freeze_level=freeze_level, learning_rate=args.learning_rate)
     model.summary()
@@ -86,7 +97,7 @@ def _main(args):
     assert (input_shape[0]%32 == 0 and input_shape[1]%32 == 0), 'Multiples of 32 required'
     batch_size = args.batch_size
     initial_epoch = 0
-    epochs = 20
+    epochs = args.init_epoch
     print('Train on {} samples, val on {} samples, with batch size {}, input_shape {}.'.format(num_train, num_val, batch_size, input_shape))
     model.fit_generator(data_generator_wrapper(lines[:num_train], batch_size, input_shape, anchors, num_classes),
             steps_per_epoch=max(1, num_train//batch_size),
@@ -104,7 +115,7 @@ def _main(args):
 
     # Do multi-scale training on different input shape
     # change every 20 epochs
-    for epoch_step in range(20, 300, 20):
+    for epoch_step in range(epochs, args.total_epoch, 20):
         input_shape = input_shape_list[random.randint(0,len(input_shape_list)-1)]
         batch_size = batch_size_list[random.randint(0,len(batch_size_list)-1)]
         initial_epoch = epochs
@@ -124,26 +135,37 @@ def _main(args):
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
+    # Model definition options
     parser.add_argument('--model_type', type=str, required=False, default='mobilenet_lite',
         help='YOLO model type: mobilenet_lite/mobilenet/darknet/vgg16/xception/xception_lite, default=mobilenet_lite')
     parser.add_argument('--tiny_version', default=False, action="store_true",
         help='Whether to use a tiny YOLO version')
-    parser.add_argument('--annotation_file', type=str, required=False, default='trainval.txt',
-        help='train&val annotation txt file, default=trainval.txt')
-    parser.add_argument('--classes_path', type=str, required=False, default='model_data/voc_classes.txt',
-        help='path to class definitions, default=model_data/voc_classes.txt')
+    parser.add_argument('--model_image_size', type=str,required=False, default='416x416',
+        help = "Initial model image input size as <num>x<num>, default 416x416")
     parser.add_argument('--weights_path', type=str,required=False, default=None,
         help = "Pretrained model/weights file for fine tune")
+    parser.add_argument('--freeze_level', type=int,required=False, default=None,
+        help = "Freeze level of the training model. 0:NA/1:backbone/2:only open feature_map")
+
+    # Data options
+    parser.add_argument('--annotation_file', type=str, required=False, default='trainval.txt',
+        help='train&val annotation txt file, default=trainval.txt')
+    parser.add_argument('--val_split', type=float,required=False, default=0.1,
+        help = "validation data persentage in dataset, default=0.1")
+    parser.add_argument('--classes_path', type=str, required=False, default='model_data/voc_classes.txt',
+        help='path to class definitions, default=model_data/voc_classes.txt')
+
+    # Training options
     parser.add_argument('--learning_rate', type=float,required=False, default=1e-3,
         help = "Initial learning rate, default=0.001")
     parser.add_argument('--batch_size', type=int,required=False, default=16,
         help = "Initial batch size for train, default=16")
-    parser.add_argument('--freeze_level', type=int,required=False, default=None,
-        help = "Freeze level of the training model. 0:NA/1:backbone/2:only open feature_map")
-    parser.add_argument('--val_split', type=float,required=False, default=0.1,
-        help = "validation data persentage in dataset, default=0.1")
-    parser.add_argument('--model_image_size', type=str,required=False, default='416x416',
-        help = "Initial model image input size as <num>x<num>, default 416x416")
+    parser.add_argument('--init_epoch', type=int,required=False, default=40,
+        help = "Initial stage training epochs, default=40")
+    parser.add_argument('--total_epoch', type=int,required=False, default=300,
+        help = "Total training epochs, default=300")
+    parser.add_argument('--multiscale', default=False, action="store_true",
+        help='Whether to use multiscale training')
 
     args = parser.parse_args()
     height, width = args.model_image_size.split('x')
