731618069b1bc90fd2dad5edcdb1dee213ab33b8
Xiaobin Zhang <david8862@gmail.com>
2019-08-26 18:39:20 +0800

    code clean

--- tools/voc_annotation.py
+++ tools/voc_annotation.py
@@ -24,16 +24,34 @@ def convert_annotation(dataset_path, year, image_id, list_file):
         list_file.write(" " + ",".join([str(a) for a in b]) + ',' + str(cls_id))
 
 
+def has_object(dataset_path, year, image_id):
+    in_file = open('%s/VOC%s/Annotations/%s.xml'%(dataset_path, year, image_id))
+    tree=ET.parse(in_file)
+    root = tree.getroot()
+    count = 0
+
+    for obj in root.iter('object'):
+        difficult = obj.find('difficult').text
+        cls = obj.find('name').text
+        if cls not in classes or int(difficult)==1:
+            continue
+        count = count +1
+    return count != 0
+
+
 parser = argparse.ArgumentParser()
 parser.add_argument('--dataset_path', type=str, help='path to PascalVOC dataset, default is ../VOCdevkit', default=getcwd()+'/../VOCdevkit')
 parser.add_argument('--output_path', type=str,  help='output path for generated annotation txt files, default is ./', default='./')
 args = parser.parse_args()
 
+
 for year, image_set in sets:
     image_ids = open('%s/VOC%s/ImageSets/Main/%s.txt'%(args.dataset_path, year, image_set)).read().strip().split()
     list_file = open('%s/%s_%s.txt'%(args.output_path, year, image_set), 'w')
     for image_id in image_ids:
-        list_file.write('%s/VOC%s/JPEGImages/%s.jpg'%(args.dataset_path, year, image_id))
-        convert_annotation(args.dataset_path, year, image_id, list_file)
-        list_file.write('\n')
+        if has_object(args.dataset_path, year, image_id):
+            list_file.write('%s/VOC%s/JPEGImages/%s.jpg'%(args.dataset_path, year, image_id))
+            convert_annotation(args.dataset_path, year, image_id, list_file)
+            list_file.write('\n')
     list_file.close()
+
