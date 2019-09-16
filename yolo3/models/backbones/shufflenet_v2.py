c4d235ef65a27ae59a65123f7c08305b5d2a5631
Xiaobin Zhang <david8862@gmail.com>
2019-09-17 00:37:47 +0800

    prepare shufflenet_v2 backbone

---
+++ yolo3/models/backbones/shufflenet_v2.py
@@ -0,0 +1,252 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+import numpy as np
+from keras_applications.imagenet_utils import _obtain_input_shape
+
+#from tensorflow.keras.utils import plot_model
+from tensorflow.keras.utils import get_source_inputs, get_file
+from tensorflow.keras.layers import Input, Conv2D, MaxPool2D, GlobalMaxPooling2D, GlobalAveragePooling2D
+from tensorflow.keras.layers import Activation, Dense
+from tensorflow.keras.layers import Add, Concatenate
+from tensorflow.keras.layers import AveragePooling2D, BatchNormalization, Lambda, DepthwiseConv2D
+from tensorflow.keras.models import Model
+
+from tensorflow.keras import backend as K
+
+# TODO prepare an imagenet pretrained weights
+BASE_WEIGHT_PATH = ('https://github.com/JonathanCMitchell/mobilenet_v2_keras/'
+                    'releases/download/v1.1/')
+
+def channel_split(x, name=''):
+    # equipartition
+    in_channles = x.shape.as_list()[-1]
+    ip = in_channles // 2
+    c_hat = Lambda(lambda z: z[:, :, :, 0:ip], name='%s/sp%d_slice' % (name, 0))(x)
+    c = Lambda(lambda z: z[:, :, :, ip:], name='%s/sp%d_slice' % (name, 1))(x)
+    return c_hat, c
+
+def channel_shuffle1(x):
+    height, width, channels = x.shape.as_list()[1:]
+    channels_per_split = channels // 2
+    x = K.reshape(x, [-1, height, width, 2, channels_per_split])
+    x = K.permute_dimensions(x, (0,1,2,4,3))
+    x = K.reshape(x, [-1, height, width, channels])
+    return x
+
+def channel_shuffle(x):
+    height, width, channels = x.shape.as_list()[1:]
+    #handle dynamic image shape case
+    if height is None:
+        height = K.shape(x)[1]
+    if width is None:
+        width = K.shape(x)[2]
+
+    channels_per_split = channels // 2
+    x = K.reshape(x, [-1, height, width, 2, channels_per_split])
+    x = K.permute_dimensions(x, (0,1,2,4,3))
+    x = K.reshape(x, [-1, height, width, channels])
+    return x
+
+
+def shuffle_unit(inputs, out_channels, bottleneck_ratio,strides=2,stage=1,block=1):
+    if K.image_data_format() == 'channels_last':
+        bn_axis = -1
+    else:
+        raise ValueError('Only channels last supported')
+
+    prefix = 'stage{}/block{}'.format(stage, block)
+    bottleneck_channels = int(out_channels * bottleneck_ratio)
+    if strides < 2:
+        c_hat, c = channel_split(inputs, '{}/spl'.format(prefix))
+        inputs = c
+
+    x = Conv2D(bottleneck_channels, kernel_size=(1,1), strides=1, padding='same', name='{}/1x1conv_1'.format(prefix))(inputs)
+    x = BatchNormalization(axis=bn_axis, name='{}/bn_1x1conv_1'.format(prefix))(x)
+    x = Activation('relu', name='{}/relu_1x1conv_1'.format(prefix))(x)
+    x = DepthwiseConv2D(kernel_size=3, strides=strides, padding='same', name='{}/3x3dwconv'.format(prefix))(x)
+    x = BatchNormalization(axis=bn_axis, name='{}/bn_3x3dwconv'.format(prefix))(x)
+    x = Conv2D(bottleneck_channels, kernel_size=1,strides=1,padding='same', name='{}/1x1conv_2'.format(prefix))(x)
+    x = BatchNormalization(axis=bn_axis, name='{}/bn_1x1conv_2'.format(prefix))(x)
+    x = Activation('relu', name='{}/relu_1x1conv_2'.format(prefix))(x)
+
+    if strides < 2:
+        ret = Concatenate(axis=bn_axis, name='{}/concat_1'.format(prefix))([x, c_hat])
+    else:
+        s2 = DepthwiseConv2D(kernel_size=3, strides=2, padding='same', name='{}/3x3dwconv_2'.format(prefix))(inputs)
+        s2 = BatchNormalization(axis=bn_axis, name='{}/bn_3x3dwconv_2'.format(prefix))(s2)
+        s2 = Conv2D(bottleneck_channels, kernel_size=1,strides=1,padding='same', name='{}/1x1_conv_3'.format(prefix))(s2)
+        s2 = BatchNormalization(axis=bn_axis, name='{}/bn_1x1conv_3'.format(prefix))(s2)
+        s2 = Activation('relu', name='{}/relu_1x1conv_3'.format(prefix))(s2)
+        ret = Concatenate(axis=bn_axis, name='{}/concat_2'.format(prefix))([x, s2])
+
+    ret = Lambda(channel_shuffle, name='{}/channel_shuffle'.format(prefix))(ret)
+
+    return ret
+
+
+def block(x, channel_map, bottleneck_ratio, repeat=1, stage=1):
+    x = shuffle_unit(x, out_channels=channel_map[stage-1],
+                      strides=2,bottleneck_ratio=bottleneck_ratio,stage=stage,block=1)
+
+    for i in range(1, repeat+1):
+        x = shuffle_unit(x, out_channels=channel_map[stage-1],strides=1,
+                          bottleneck_ratio=bottleneck_ratio,stage=stage, block=(1+i))
+
+    return x
+
+
+def ShuffleNetV2(input_shape=None,
+                 include_top=True,
+                 weights='imagenet',
+                 input_tensor=None,
+                 scale_factor=1.0,
+                 pooling=None,
+                 num_shuffle_units=[3,7,3],
+                 bottleneck_ratio=1,
+                 classes=1000,
+                 **kwargs):
+    """Instantiates the ShuffleNetV2 architecture.
+
+    # Arguments
+        input_shape: optional shape tuple, to be specified if you would
+            like to use a model with an input img resolution that is not
+            (224, 224, 3).
+            It should have exactly 3 inputs channels (224, 224, 3).
+            You can also omit this option if you would like
+            to infer input_shape from an input_tensor.
+            If you choose to include both input_tensor and input_shape then
+            input_shape will be used if they match, if the shapes
+            do not match then we will throw an error.
+            E.g. `(160, 160, 3)` would be one valid value.
+        include_top: whether to include the fully-connected
+            layer at the top of the network.
+        weights: one of `None` (random initialization),
+              'imagenet' (pre-training on ImageNet),
+              or the path to the weights file to be loaded.
+        input_tensor: optional Keras tensor (i.e. output of
+            `layers.Input()`)
+            to use as image input for the model.
+        pooling: Optional pooling mode for feature extraction
+            when `include_top` is `False`.
+            - `None` means that the output of the model
+                will be the 4D tensor output of the
+                last convolutional block.
+            - `avg` means that global average pooling
+                will be applied to the output of the
+                last convolutional block, and thus
+                the output of the model will be a
+                2D tensor.
+            - `max` means that global max pooling will
+                be applied.
+        classes: optional number of classes to classify images
+            into, only to be specified if `include_top` is True, and
+            if no `weights` argument is specified.
+
+    # Returns
+        A Keras model instance.
+
+    # Raises
+        ValueError: in case of invalid argument for `weights`,
+            or invalid input shape or invalid alpha, rows when
+            weights='imagenet'
+    """
+    if K.backend() != 'tensorflow':
+        raise RuntimeError('Only tensorflow supported for now')
+    name = 'ShuffleNetV2_{}_{}_{}'.format(scale_factor, bottleneck_ratio, "".join([str(x) for x in num_shuffle_units]))
+    input_shape = _obtain_input_shape(input_shape, default_size=224, min_size=28, require_flatten=include_top,
+                                      data_format=K.image_data_format())
+    out_dim_stage_two = {0.5:48, 1:116, 1.5:176, 2:244}
+
+    if pooling not in ['max', 'avg', None]:
+        raise ValueError('Invalid value for pooling')
+    if not (float(scale_factor)*4).is_integer():
+        raise ValueError('Invalid value for scale_factor, should be x over 4')
+    exp = np.insert(np.arange(len(num_shuffle_units), dtype=np.float32), 0, 0)  # [0., 0., 1., 2.]
+    out_channels_in_stage = 2**exp
+    out_channels_in_stage *= out_dim_stage_two[bottleneck_ratio]  #  calculate output channels for each stage
+    out_channels_in_stage[0] = 24  # first stage has always 24 output channels
+    out_channels_in_stage *= scale_factor
+    out_channels_in_stage = out_channels_in_stage.astype(int)
+
+    if input_tensor is None:
+        img_input = Input(shape=input_shape)
+    else:
+        #if not K.is_keras_tensor(input_tensor):
+            #img_input = Input(tensor=input_tensor, shape=input_shape)
+        #else:
+            #img_input = input_tensor
+        img_input = input_tensor
+
+    # create shufflenet architecture
+    x = Conv2D(filters=out_channels_in_stage[0], kernel_size=(3, 3), padding='same', use_bias=False, strides=(2, 2),
+               activation='relu', name='conv1')(img_input)
+    x = MaxPool2D(pool_size=(3, 3), strides=(2, 2), padding='same', name='maxpool1')(x)
+
+    # create stages containing shufflenet units beginning at stage 2
+    for stage in range(len(num_shuffle_units)):
+        repeat = num_shuffle_units[stage]
+        x = block(x, out_channels_in_stage,
+                   repeat=repeat,
+                   bottleneck_ratio=bottleneck_ratio,
+                   stage=stage + 2)
+
+    if bottleneck_ratio < 2:
+        k = 1024
+    else:
+        k = 2048
+    x = Conv2D(k, kernel_size=1, padding='same', strides=1, name='1x1conv5_out', activation='relu')(x)
+
+
+    if include_top:
+        x = GlobalAveragePooling2D(name='global_avg_pool')(x)
+        x = Dense(classes, activation='softmax',
+                  use_bias=True, name='Logits')(x)
+    else:
+        if pooling == 'avg':
+            x = GlobalAveragePooling2D(name='global_avg_pool')(x)
+        elif pooling == 'max':
+            x = GlobalMaxPooling2D(name='global_max_pool')(x)
+
+
+    # Ensure that the model takes into account
+    # any potential predecessors of `input_tensor`.
+    if input_tensor is not None:
+        inputs = get_source_inputs(input_tensor)
+
+    else:
+        inputs = img_input
+
+    # Create model.
+    model = Model(inputs, x, name=name)
+
+    # Load weights.
+    if weights == 'imagenet':
+        if K.image_data_format() == 'channels_first':
+            raise ValueError('Weights for "channels_first" format '
+                             'are not available.')
+
+        if include_top:
+            model_name = ('shufflenet_v2_weights_tf_dim_ordering_tf_kernels_' +
+                          str(alpha) + '_' + str(rows) + '.h5')
+            weigh_path = BASE_WEIGHT_PATH + model_name
+            weights_path = get_file(
+                model_name, weigh_path, cache_subdir='models')
+        else:
+            model_name = ('shufflenet_v2_weights_tf_dim_ordering_tf_kernels_' +
+                          str(alpha) + '_' + str(rows) + '_no_top' + '.h5')
+            weigh_path = BASE_WEIGHT_PATH + model_name
+            weights_path = get_file(
+                model_name, weigh_path, cache_subdir='models')
+        model.load_weights(weights_path)
+    elif weights is not None:
+        model.load_weights(weights)
+
+    return model
+
+
+if __name__ == '__main__':
+    input_tensor = Input(shape=(None, None, 3), name='image_input')
+    #model = ShuffleNetV2(include_top=False, input_shape=(224, 224, 3), weights=None, bottleneck_ratio=1)
+    model = ShuffleNetV2(include_top=False, input_tensor=input_tensor, weights=None, bottleneck_ratio=1)
+    model.summary()
+    #plot_model(model, to_file='shufflenetv2.png', show_layer_names=True, show_shapes=True)
