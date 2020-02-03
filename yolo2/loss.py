5972832c5aa6e205df980172e9ab5dc0da8a57b9
Xiaobin Zhang <david8862@gmail.com>
2020-02-03 22:48:05 +0800

    draft to add more loss support for YOLOv2

--- yolo2/loss.py
+++ yolo2/loss.py
@@ -1,6 +1,7 @@
 # -*- coding=utf-8 -*-
 #!/usr/bin/python3
 
+import math
 from tensorflow.keras import backend as K
 from yolo2.postprocess import yolo2_head
 
@@ -45,12 +46,70 @@ def box_iou(b1, b2):
     return iou
 
 
+def box_diou(b1, b2):
+    """
+    Calculate DIoU loss on anchor boxes
+    Reference Paper:
+        "Distance-IoU Loss: Faster and Better Learning for Bounding Box Regression"
+        https://arxiv.org/abs/1911.08287
+
+    Parameters
+    ----------
+    b1: tensor, shape=(batch, feat_w, feat_h, anchor_num, 4), xywh
+    b2: tensor, shape=(batch, feat_w, feat_h, anchor_num, 4), xywh
+
+    Returns
+    -------
+    diou: tensor, shape=(batch, feat_w, feat_h, anchor_num, 1)
+    """
+    b1_xy = b1[..., :2]
+    b1_wh = b1[..., 2:4]
+    b1_wh_half = b1_wh/2.
+    b1_mins = b1_xy - b1_wh_half
+    b1_maxes = b1_xy + b1_wh_half
+
+    b2_xy = b2[..., :2]
+    b2_wh = b2[..., 2:4]
+    b2_wh_half = b2_wh/2.
+    b2_mins = b2_xy - b2_wh_half
+    b2_maxes = b2_xy + b2_wh_half
+
+    intersect_mins = K.maximum(b1_mins, b2_mins)
+    intersect_maxes = K.minimum(b1_maxes, b2_maxes)
+    intersect_wh = K.maximum(intersect_maxes - intersect_mins, 0.)
+    intersect_area = intersect_wh[..., 0] * intersect_wh[..., 1]
+    b1_area = b1_wh[..., 0] * b1_wh[..., 1]
+    b2_area = b2_wh[..., 0] * b2_wh[..., 1]
+    union_area = b1_area + b2_area - intersect_area
+    # calculate IoU, add epsilon in denominator to avoid dividing by 0
+    iou = intersect_area / (union_area + K.epsilon())
+
+    # box center distance
+    center_distance = K.sum(K.square(b1_xy - b2_xy), axis=-1)
+    # get enclosed area
+    enclose_mins = K.minimum(b1_mins, b2_mins)
+    enclose_maxes = K.maximum(b1_maxes, b2_maxes)
+    enclose_wh = K.maximum(enclose_maxes - enclose_mins, 0.0)
+    # get enclosed diagonal distance
+    enclose_diagonal = K.sum(K.square(enclose_wh), axis=-1)
+    # calculate DIoU, add epsilon in denominator to avoid dividing by 0
+    diou = iou - 1.0 * (center_distance) / (enclose_diagonal + K.epsilon())
+
+    # calculate param v and alpha to extend to CIoU
+    #v = 4*K.square(tf.math.atan2(b1_wh[..., 0], b1_wh[..., 1]) - tf.math.atan2(b2_wh[..., 0], b2_wh[..., 1])) / (math.pi * math.pi)
+    #alpha = v / (1.0 - iou + v)
+    #diou = diou - alpha*v
+
+    diou = K.expand_dims(diou, -1)
+    return diou
+
+
 def _smooth_labels(y_true, label_smoothing):
     label_smoothing = K.constant(label_smoothing, dtype=K.floatx())
     return y_true * (1.0 - label_smoothing) + 0.5 * label_smoothing
 
 
-def yolo2_loss(args, anchors, num_classes, label_smoothing=0, use_crossentropy_loss=False, use_crossentropy_obj_loss=False, rescore_confidence=False):
+def yolo2_loss(args, anchors, num_classes, label_smoothing=0, use_crossentropy_loss=False, use_crossentropy_obj_loss=False, rescore_confidence=False, use_diou_loss=False):
     """YOLOv2 loss function.
 
     Parameters
@@ -89,7 +148,7 @@ def yolo2_loss(args, anchors, num_classes, label_smoothing=0, use_crossentropy_l
     object_scale = 5
     no_object_scale = 1
     class_scale = 1
-    coordinates_scale = 1
+    location_scale = 1
 
     pred_xy, pred_wh, pred_confidence, pred_class_prob = yolo2_head(
         yolo_output, anchors, num_classes, input_shape)
@@ -158,30 +217,37 @@ def yolo2_loss(args, anchors, num_classes, label_smoothing=0, use_crossentropy_l
         classification_loss = (class_scale * object_mask *
                            K.square(matching_classes - pred_class_prob))
 
-    # Coordinate loss for matching detection boxes.
-    matching_boxes = y_true[..., 0:4]
+    if use_diou_loss:
+        # Calculate DIoU loss as location loss
+        diou = box_diou(pred_boxes, true_boxes)
+        diou = K.squeeze(diou, axis=-1)
+        diou_loss = location_scale * object_mask * (1 - diou)
+        location_loss = diou_loss
+    else:
+        # YOLOv2 location loss for matching detection boxes.
+        matching_boxes = y_true[..., 0:4]
 
-    feats = K.reshape(yolo_output, [
-        -1, yolo_output_shape[1], yolo_output_shape[2], num_anchors,
-        num_classes + 5
-    ])
-    # Unadjusted box predictions for loss.
-    # TODO: Remove extra computation shared with yolo2_head.
-    raw_pred_boxes = K.concatenate(
-        (K.sigmoid(feats[..., 0:2]), feats[..., 2:4]), axis=-1)
+        feats = K.reshape(yolo_output, [
+            -1, yolo_output_shape[1], yolo_output_shape[2], num_anchors,
+            num_classes + 5
+        ])
+        # Unadjusted box predictions for loss.
+        # TODO: Remove extra computation shared with yolo2_head.
+        raw_pred_boxes = K.concatenate(
+            (K.sigmoid(feats[..., 0:2]), feats[..., 2:4]), axis=-1)
 
-    coordinates_loss = (coordinates_scale * object_mask *
-                        K.square(matching_boxes - raw_pred_boxes))
+        location_loss = (location_scale * object_mask *
+                            K.square(matching_boxes - raw_pred_boxes))
 
 
     confidence_loss_sum = K.sum(confidence_loss) / batch_size_f
     classification_loss_sum = K.sum(classification_loss) / batch_size_f
-    coordinates_loss_sum = K.sum(coordinates_loss) / batch_size_f
+    location_loss_sum = K.sum(location_loss) / batch_size_f
     total_loss = 0.5 * (
-        confidence_loss_sum + classification_loss_sum + coordinates_loss_sum)
+        confidence_loss_sum + classification_loss_sum + location_loss_sum)
 
     # Fit for tf 2.0.0 loss shape
     total_loss = K.expand_dims(total_loss, axis=-1)
 
-    return total_loss, coordinates_loss_sum, confidence_loss_sum, classification_loss_sum
+    return total_loss, location_loss_sum, confidence_loss_sum, classification_loss_sum
 
