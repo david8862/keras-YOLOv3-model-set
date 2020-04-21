8f926f984ef6a4423a6c4bf7403bd223ece3d033
Xiaobin Zhang <david8862@gmail.com>
2020-04-21 18:12:44 +0800

    fix division by zero error in eval.py

--- eval.py
+++ eval.py
@@ -378,7 +378,7 @@ def box_iou(pred_box, gt_box):
     gt_area = (gt_box[2] - gt_box[0] + 1) * (gt_box[3] - gt_box[1] + 1)
     inter_area = inter_w * inter_h
     union_area = pred_area + gt_area - inter_area
-    return float(inter_area) / float(union_area)
+    return 0 if union_area == 0 else float(inter_area) / float(union_area)
 
 
 def match_gt_box(pred_record, gt_records, iou_threshold=0.5):
