fix index overflow in rotate augment

common/data_utils.py
@@ -450,36 +450,48 @@ def random_rotate(image, boxes, rotate_range=20, prob=0.1):
         width, height = image.size
         scale = 1.0
 
+        # get rotate matrix and apply for image
         M = cv2.getRotationMatrix2D(center=(width//2, height//2), angle=angle, scale=scale)
         img = cv2.warpAffine(np.array(image), M, (width, height), flags=cv2.INTER_NEAREST, borderMode=cv2.BORDER_CONSTANT, borderValue=0)#, borderValue=(114, 114, 114))
 
-        # Rotate boxes coordinates
+        # rotate boxes coordinates
         n = len(boxes)
         if n:
-            # warp points
-            xy = np.ones((n * 4, 3))
-            xy[:, :2] = boxes[:, [0, 1, 2, 3, 0, 3, 2, 1]].reshape(n * 4, 2) # x1y1, x2y2, x1y2, x2y1
-            xy = xy @ M.T  # transform
-            xy = xy[:, :2].reshape(n, 8)
+            # form up 4 corner points ([xmin,ymin], [xmax,ymax], [xmin,ymax], [xmax,ymin])
+            # from (xmin, ymin, xmax, ymax), every coord is [x,y,1] format for applying
+            # rotation matrix
+            corner_points = np.ones((n * 4, 3))
+            corner_points[:, :2] = boxes[:, [0, 1, 2, 3, 0, 3, 2, 1]].reshape(n * 4, 2) # [xmin,ymin], [xmax,ymax], [xmin,ymax], [xmax,ymin]
 
-            # create new boxes
-            x = xy[:, [0, 2, 4, 6]]
-            y = xy[:, [1, 3, 5, 7]]
-            xy = np.concatenate((x.min(1), y.min(1), x.max(1), y.max(1))).reshape(4, n).T
+            # apply rotation transform
+            corner_points = corner_points @ M.T
 
-            # clip boxes
-            xy[:, [0, 2]] = xy[:, [0, 2]].clip(0, width)
-            xy[:, [1, 3]] = xy[:, [1, 3]].clip(0, height)
+            # pick rotated corner (x,y) and reshape to 1 column
+            corner_points = corner_points[:, :2].reshape(n, 8)
+            # select x lines and y lines
+            corner_x = corner_points[:, [0, 2, 4, 6]]
+            corner_y = corner_points[:, [1, 3, 5, 7]]
 
-            boxes[:, :4] = xy
+            # create new bounding boxes according to rotated corner points boundary
+            rotate_boxes = np.concatenate((corner_x.min(axis=-1), corner_y.min(axis=-1), corner_x.max(axis=-1), corner_y.max(axis=-1))).reshape(4, n).T
+
+            # clip boxes with image size
+            # NOTE: use (width-1) & (height-1) as max to avoid index overflow
+            rotate_boxes[:, [0, 2]] = rotate_boxes[:, [0, 2]].clip(0, width-1)
+            rotate_boxes[:, [1, 3]] = rotate_boxes[:, [1, 3]].clip(0, height-1)
+
+            # apply new boxes
+            boxes[:, :4] = rotate_boxes
+
             # filter candidates
-            #i = box_candidates(box1=boxes[:, :4].T * scale, box2=xy.T)
+            #i = box_candidates(box1=boxes[:, :4].T * scale, box2=rotate_boxes.T)
             #boxes = boxes[i]
-            #boxes[:, :4] = xy[i]
+            #boxes[:, :4] = rotate_boxes[i]
 
         image = Image.fromarray(img)
 
     return image, boxes
+
 
 
 def box_candidates(box1, box2, wh_thr=2, ar_thr=20, area_thr=0.1):  # box1(4,n), box2(4,n)

yolo2/data.py
@@ -52,7 +52,7 @@ def preprocess_true_boxes(true_boxes, anchors, input_shape, num_classes, multi_a
         y_true feature map array with shape [conv_height, conv_width, num_anchors, 6]
         in form of relative x, y, w, h, objectness, class
     """
-    assert (true_boxes[..., 4]<num_classes).all(), 'class id must be less than num_classes'
+    assert (true_boxes[..., 4] < num_classes).all(), 'class id must be less than num_classes'
     height, width = input_shape
     num_anchors = len(anchors)
     # Downsampling factor of 5x 2-stride max_pools == 32.
@@ -68,8 +68,8 @@ def preprocess_true_boxes(true_boxes, anchors, input_shape, num_classes, multi_a
     input_shape = np.array(input_shape, dtype='int32')
     boxes_xy = (true_boxes[..., 0:2] + true_boxes[..., 2:4]) // 2
     boxes_wh = true_boxes[..., 2:4] - true_boxes[..., 0:2]
-    true_boxes[..., 0:2] = boxes_xy/input_shape[::-1]
-    true_boxes[..., 2:4] = boxes_wh/input_shape[::-1]
+    true_boxes[..., 0:2] = boxes_xy / input_shape[::-1]
+    true_boxes[..., 2:4] = boxes_wh / input_shape[::-1]
 
     num_box_params = true_boxes.shape[1]
     y_true = np.zeros(
@@ -78,18 +78,18 @@ def preprocess_true_boxes(true_boxes, anchors, input_shape, num_classes, multi_a
 
     for box in true_boxes:
         # bypass invalid true_box, if w,h is 0
-        if box[2]==0 and box[3]==0:
+        if box[2] == 0 and box[3] == 0:
             continue
 
         box_class = box[4:5]
-        i = np.floor(box[1]*conv_height).astype('int')
-        j = np.floor(box[0]*conv_width).astype('int')
+        i = np.floor(box[1] * conv_height).astype('int')
+        j = np.floor(box[0] * conv_width).astype('int')
         best_iou = 0
         #best_anchor = 0
         selected_anchors = []
         for k, anchor in enumerate(anchors):
             # Find IOU between box shifted to origin and anchor box.
-            box_maxes = box[2:4]*input_shape[::-1] / 2.
+            box_maxes = box[2:4] * input_shape[::-1] / 2.
             box_mins = -box_maxes
             anchor_maxes = (anchor / 2.)
             anchor_mins = -anchor_maxes
@@ -98,7 +98,7 @@ def preprocess_true_boxes(true_boxes, anchors, input_shape, num_classes, multi_a
             intersect_maxes = np.minimum(box_maxes, anchor_maxes)
             intersect_wh = np.maximum(intersect_maxes - intersect_mins, 0.)
             intersect_area = intersect_wh[0] * intersect_wh[1]
-            box_area = (box[2]*input_shape[1]) * (box[3]*input_shape[0])
+            box_area = (box[2] * input_shape[1]) * (box[3] * input_shape[0])
             anchor_area = anchor[0] * anchor[1]
             iou = intersect_area / (box_area + anchor_area - intersect_area)
 

yolo3/data.py
@@ -106,7 +106,7 @@ def preprocess_true_boxes(true_boxes, input_shape, anchors, num_classes, multi_a
     y_true: list of array, shape like yolo_outputs, xywh are reletive value
 
     '''
-    assert (true_boxes[..., 4]<num_classes).all(), 'class id must be less than num_classes'
+    assert (true_boxes[..., 4] < num_classes).all(), 'class id must be less than num_classes'
     num_layers = len(anchors)//3 # default setting
     anchor_mask = [[6,7,8], [3,4,5], [0,1,2]] if num_layers==3 else [[3,4,5], [0,1,2]]
 
@@ -116,24 +116,24 @@ def preprocess_true_boxes(true_boxes, input_shape, anchors, num_classes, multi_a
     input_shape = np.array(input_shape, dtype='int32')
     boxes_xy = (true_boxes[..., 0:2] + true_boxes[..., 2:4]) // 2
     boxes_wh = true_boxes[..., 2:4] - true_boxes[..., 0:2]
-    true_boxes[..., 0:2] = boxes_xy/input_shape[::-1]
-    true_boxes[..., 2:4] = boxes_wh/input_shape[::-1]
+    true_boxes[..., 0:2] = boxes_xy / input_shape[::-1]
+    true_boxes[..., 2:4] = boxes_wh / input_shape[::-1]
 
     batch_size = true_boxes.shape[0]
     grid_shapes = [input_shape//{0:32, 1:16, 2:8}[l] for l in range(num_layers)]
-    y_true = [np.zeros((batch_size, grid_shapes[l][0], grid_shapes[l][1], len(anchor_mask[l]), 5+num_classes),
+    y_true = [np.zeros((batch_size, grid_shapes[l][0], grid_shapes[l][1], len(anchor_mask[l]), 5 + num_classes),
         dtype='float32') for l in range(num_layers)]
 
     # Expand dim to apply broadcasting.
     anchors = np.expand_dims(anchors, 0)
     anchor_maxes = anchors / 2.
     anchor_mins = -anchor_maxes
-    valid_mask = boxes_wh[..., 0]>0
+    valid_mask = boxes_wh[..., 0] > 0
 
     for b in range(batch_size):
         # Discard zero rows.
         wh = boxes_wh[b, valid_mask[b]]
-        if len(wh)==0:
+        if len(wh) == 0:
             continue
 
         # Expand dim to apply broadcasting.
@@ -168,11 +168,11 @@ def preprocess_true_boxes(true_boxes, input_shape, anchors, num_classes, multi_a
                         matching_rule = (n in anchor_mask[l])
 
                     if matching_rule:
-                        i = np.floor(true_boxes[b,t,0]*grid_shapes[l][1]).astype('int32')
-                        j = np.floor(true_boxes[b,t,1]*grid_shapes[l][0]).astype('int32')
+                        i = np.floor(true_boxes[b, t, 0] * grid_shapes[l][1]).astype('int32')
+                        j = np.floor(true_boxes[b, t, 1] * grid_shapes[l][0]).astype('int32')
                         k = anchor_mask[l].index(n)
-                        c = true_boxes[b,t, 4].astype('int32')
-                        y_true[l][b, j, i, k, 0:4] = true_boxes[b,t, 0:4]
+                        c = true_boxes[b, t, 4].astype('int32')
+                        y_true[l][b, j, i, k, 0:4] = true_boxes[b, t, 0:4]
                         y_true[l][b, j, i, k, 4] = 1
                         y_true[l][b, j, i, k, 5+c] = 1
 